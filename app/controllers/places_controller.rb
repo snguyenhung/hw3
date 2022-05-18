0579bb292aea967bfc7dddbc6205966ccd0a9dda
snguyenhung <hung.nguyen@kellogg.northwestern.edu>
2022-05-18 22:19:26 +0000

    made my app more perfect

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,26 +1,24 @@
 class PlacesController < ApplicationController
 
     def index
-      @places = Place.all
-    end
-  
-    def show
-      @place = Place.find_by({ "id" => params["id"] })
-      @posts = Post.where({ "place_id" => @place["id"] })
-    end
-  
-    def new
-      @place = Place.new
-    end
-  
-    def create
-      @place = Place.new
-      @place["name"] = params["place"]["name"]
-      @company.save
-      redirect_to "/places"
-    end
-  
+        @places = Place.all
+      end
     
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.where({ "place_id" => @place["id"] })
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
   
-  end
-  
+
+end
